cd120cabd3cc802d8e084970bfdb7676b745e9ca
Andrea Thompson <athompson314@yahoo.com>
2025-09-24 10:32:39 -0500

    character check practice

--- practice-character-check.js
+++ practice-character-check.js
@@ -1,1 +1,8 @@
+const readline = require('readline-sync');
 
+let firstName = readline.question("What is your first name? ");
+let validIndex = firstName.length - 1;
+let index = readline.question("Please choose a number between 0 and " + validIndex + ".");
+console.log("validIndex is " + validIndex);
+
+console.log("The number you chose, " + index + " corresponds wtih the letter " + firstName[index] + " of your name.");
